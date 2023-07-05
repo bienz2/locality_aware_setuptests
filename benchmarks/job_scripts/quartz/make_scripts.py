69a35859d3cec4faf80bcb7c069c503c5318bd19
Andrew Valeri Geyko <geyko2@quartz1154.llnl.gov>
2023-07-05 15:16:28 -0700

    ran average tests

--- benchmarks/job_scripts/quartz/make_scripts.py
+++ benchmarks/job_scripts/quartz/make_scripts.py
@@ -24,6 +24,22 @@ def Create_Power_Two_Tests(num_tests : int, m_name : str):
       for algo in ["STANDARD", "TORSTEN", "RMA"]:
         fp.write(f"srun --partition=pbatch --nodes={math.ceil((2**(j+1))/36)} --ntasks={(2**(j+1))} --output {f_path}/{m_name}/data/output/{m_name}_QUARTZ_{algo}_many_node --error {f_path}/{m_name}/data/error/{m_name}_QUARTZ_{algo}_many_node_err ../../../build_quartz/benchmarks/comm_creators ../../../test_data/{m_name}.pm 1 {m_name} {algo}\n")
 
+def Create_Varied_Power_Two_Tests(m_name : str, test_range : int):
+  fp = open(f"{m_name}_QUARTZ_VARIED_POWER_TWO.sh","w")
+  fp.write("#!/usr/bin/bash\n")
+  fp.write("#SBATCH --partition pbatch")
+  fp.write(f"#SBATCH --open-mode=append\n")
+  fp.write("#SBATCH --ntasks=2048\n")
+  fp.write("#SBATCH --nodes=64\n\n")
+  fp.write("#SBATCH --mail-type=BEGIN,FAIL,END\n")
+  fp.write("#SBATCH --mail-user=ageyko@unm.edu\n\n")
+  fp.write("module load openmpi\n\n")
+  for j in range(11):
+      for i in range(1, test_range):
+          for algo in ["STANDARD", "TORSTEN", "RMA"]:
+            fp.write(f"srun --time=24:00:00 --partition=pbatch --nodes={math.ceil((2**(j+1))/36)} --ntasks={(2**(j+1))} --output {f_path}/{m_name}/data/output/{m_name}_QUARTZ_{algo}_varied_runs --error {f_path}/{m_name}/data/error/{m_name}_QUARTZ_{algo}_varied_runs_err ../../../build_quartz/benchmarks/comm_creators ../../../test_data/{m_name}.pm {2**i} {m_name} {algo}\n")
+
+
 for (i, m_name) in enumerate(matrix_names):
   if(not os.path.exists(f"{f_path}/{m_name}/")):
     os.mkdir(f"{f_path}/{m_name}/")
@@ -39,4 +55,5 @@ for (i, m_name) in enumerate(matrix_names):
     os.mkdir(f"{f_path}/{m_name}/data/output")
     os.mkdir(f"{f_path}/{m_name}/data/error")
 
-  Create_Power_Two_Tests(10, m_name)
+  #Create_Power_Two_Tests(10, m_name)
+  Create_Varied_Power_Two_Tests(m_name, 7)
