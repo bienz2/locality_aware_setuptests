a8e71bf8657f6157c42bcb387cbc8054472d304a
Andrew Valeri Geyko <geyko2@quartz1538.llnl.gov>
2023-07-12 08:47:24 -0700

    ran no_window timings

--- benchmarks/job_scripts/quartz/make_scripts.py
+++ benchmarks/job_scripts/quartz/make_scripts.py
@@ -5,6 +5,10 @@ import math
 matrix_names = ['3elt','lns_3937','bcsstm13','circuit_1','ct20stif','mbeacxc','onetone1','rbsa480']
 # expanded test suite
 matrix_names = ['nemeth26', 'bundle1', 'cbuckle', 'Kuu', 'ACTIVSg70k']
+# combined test suite 
+matrix_names =  ['3elt','lns_3937','bcsstm13','circuit_1','ct20stif','mbeacxc','onetone1','rbsa480','nemeth26', 'bundle1', 'cbuckle', 'Kuu', 'ACTIVSg70k']
+
+
 f_path = '../../../benchmark_tests/comm_creation'
 mpi_type = "openmpi"
 
@@ -19,10 +23,11 @@ def Create_Power_Two_Tests(num_tests : int, m_name : str):
   fp.write("#SBATCH --mail-type=BEGIN,FAIL,END\n")
   fp.write("#SBATCH --mail-user=ageyko@unm.edu\n\n")
   fp.write("module load openmpi\n\n")
+  f_name = "RMA_DYNAMIC_NO_WINDOW"
   for j in range(11):
     for _ in range(num_tests):
-      for algo in ["STANDARD", "TORSTEN", "RMA_DYNAMIC"]:
-        fp.write(f"srun --partition=pbatch --nodes={math.ceil((2**(j+1))/36)} --ntasks={(2**(j+1))} --output {f_path}/{m_name}/data/output/{m_name}_QUARTZ_{algo}_many_node --error {f_path}/{m_name}/data/error/{m_name}_QUARTZ_{algo}_many_node_err ../../../build_quartz/benchmarks/comm_creators ../../../test_data/{m_name}.pm 1 {m_name} {algo}\n")
+      for algo in ["RMA_DYNAMIC"]:
+        fp.write(f"srun --partition=pbatch --nodes={math.ceil((2**(j+1))/36)} --ntasks={(2**(j+1))} --output {f_path}/{m_name}/data/output/{m_name}_QUARTZ_{f_name}_many_node --error {f_path}/{m_name}/data/error/{m_name}_QUARTZ_{f_name}_many_node_err ../../../build_quartz/benchmarks/comm_creators ../../../test_data/{m_name}.pm 1 {m_name} {algo}\n")
 
 def Create_Varied_Power_Two_Tests(m_name : str, test_range : int):
   fp = open(f"{m_name}_QUARTZ_VARIED_POWER_TWO.sh","w")
@@ -36,7 +41,7 @@ def Create_Varied_Power_Two_Tests(m_name : str, test_range : int):
   fp.write("module load openmpi\n\n")
   for j in range(11):
       for i in range(1, test_range):
-          for algo in ["STANDARD", "TORSTEN", "RMA_DYNAMIC"]:
+          for algo in ["RMA_DYNAMIC"]:
             fp.write(f"srun --time=24:00:00 --partition=pbatch --nodes={math.ceil((2**(j+1))/36)} --ntasks={(2**(j+1))} --output {f_path}/{m_name}/data/output/{m_name}_QUARTZ_{algo}_varied_runs --error {f_path}/{m_name}/data/error/{m_name}_QUARTZ_{algo}_varied_runs_err ../../../build_quartz/benchmarks/comm_creators ../../../test_data/{m_name}.pm {2**i} {m_name} {algo}\n")
 
 
@@ -55,5 +60,5 @@ for (i, m_name) in enumerate(matrix_names):
     os.mkdir(f"{f_path}/{m_name}/data/output")
     os.mkdir(f"{f_path}/{m_name}/data/error")
 
-  Create_Power_Two_Tests(10, m_name)
+  Create_Power_Two_Tests(5, m_name)
   #Create_Varied_Power_Two_Tests(m_name, 7)
